fd68eb13994d07f620908c1fd49ef540ad60d747
Sverre <sverre.skuland@gmail.com>
2025-06-27 08:35:06 +0200

    Create vue plugin.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import type { App } from 'vue';
+
+// Css stuff
 import './assets/icons.css';
 
-import TestComponent from './components/TestComponent.vue';
+// Components
 import Badge from './components/Badge.vue';
 import Button from './components/Button.vue';
 import ButtonToggle from './components/ButtonToggle.vue';
@@ -30,10 +33,50 @@ import Tooltip from './components/Tooltip.vue';
 import TouchNumberInput from './components/TouchNumberInput.vue';
 import TransitionGroupFadeSlide from './components/TransitionGroupFadeSlide.vue';
 
-// import DateHelpers from "./components/DateHelpers.ts";
+// Directives
+import { rippleDirective } from './directives/ripple';
+
+const install = (app: App) => {
+    app.directive('ripple', rippleDirective);
+
+    // Register components globally
+    app.component('Badge', Badge);
+    app.component('Button', Button);
+    app.component('ButtonToggle', ButtonToggle);
+    app.component('Card', Card);
+    app.component('Checkbox', Checkbox);
+    app.component('Date', Date);
+    app.component('Dialog', Dialog);
+    app.component('EllipsisText', EllipsisText);
+    app.component('ExpansionItem', ExpansionItem);
+    app.component('Fab', Fab);
+    app.component('FabAction', FabAction);
+    app.component('Header', Header);
+    app.component('Icon', Icon);
+    app.component('Input', Input);
+    app.component('MathOperationButtons', MathOperationButtons);
+    app.component('NavMenu', NavMenu);
+    app.component('PullToRefresh', PullToRefresh);
+    app.component('ScrollArea', ScrollArea);
+    app.component('Select', Select);
+    app.component('SlideItem', SlideItem);
+    app.component('Spinner', Spinner);
+    app.component('Stepper', Stepper);
+    app.component('StepperStep', StepperStep);
+    app.component('Tab', Tab);
+    app.component('Tabs', Tabs);
+    app.component('Tooltip', Tooltip);
+    app.component('TouchNumberInput', TouchNumberInput);
+    app.component('TransitionGroupFadeSlide', TransitionGroupFadeSlide);
+};
+
+const Windvane = {
+    install,
+};
+
+export default Windvane;
 
 export {
-    TestComponent,
     Badge,
     Button,
     ButtonToggle,
