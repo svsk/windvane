9440f57fcca4b20eec18911e06435cbd08097e04
Sverre <sverre.skuland@gmail.com>
2025-06-27 12:33:44 +0200

    Use Windvane in playground project.

--- playground/src/components/HelloWorld.vue
+++ playground/src/components/HelloWorld.vue
@@ -7,11 +7,8 @@ defineProps<{
 <template>
     <div class="greetings">
         <h1 class="green">{{ msg }}</h1>
-        <h3>
-            You’ve successfully created a project with
-            <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-            <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-        </h3>
+        <h3>This is project showcasing the<br />Windvane Vue UI framework.</h3>
+        <span class="opacity-50">Powered by Tailwind and inspired by Material Design.</span>
     </div>
 </template>
 
--- playground/src/main.ts
+++ playground/src/main.ts
@@ -4,7 +4,14 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+// Perform Windvane imports
+import '@skyloft/windvane/windvane.css';
+import Windvane from '@skyloft/windvane';
+
 const app = createApp(App);
+
+// Install as plugin
+app.use(Windvane);
 
 app.use(router);
 
